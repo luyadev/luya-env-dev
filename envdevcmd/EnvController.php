1451a4cf00b96bec06e7df7ad2180e180fce764a
Basil Suter <git@nadar.io>
2017-12-18 17:27:37 +0100

    improve.

--- envdevcmd/EnvController.php
+++ envdevcmd/EnvController.php
@@ -4,6 +4,9 @@ namespace luya\envdev\envdevcmd;
 
 use Curl\Curl;
 use GitWrapper\GitWrapper;
+use yii\console\widgets\Table;
+use yii\console\Markdown;
+use yii\helpers\Console;
 
 class EnvController extends BaseCommand
 {
@@ -13,52 +16,138 @@ class EnvController extends BaseCommand
         'luya-module-cms',
     ];
     
+    public $text = <<<EOT
+
+**CLONE REPOS**
+
+We have detected one or more repos which are not cloned to your account. Therefore you can push any changes to those repos and they are **READ ONLY**.
+
+If you just want to work on a specific repo, make sure at least this repo is forked into your Github account.
+
+You can also skip this command, clone the repos and re run this command again.
+
+**FORK ME**
+EOT;
+    
+    private $_gitWrapper;
+    
+    /**
+     * @return \GitWrapper\GitWrapper
+     */
+    protected function getGitWrapper()
+    {
+        if ($this->_gitWrapper === null) {
+            $this->_gitWrapper = new GitWrapper();
+        }
+        
+        return $this->_gitWrapper;
+    }
+    
+    private function summaryItem($repo, $isFork, $exists)
+    {
+        return [$repo, $exists, $isFork];
+    }
+    
+    private function getFilesystemRepoPath($repo)
+    {
+        return 'repos' . DIRECTORY_SEPARATOR . $repo;
+    }
+
+    private function forkExists($username, $repo)
+    {
+        return (new Curl())->get('https://api.github.com/repos/'.$username.'/'.$repo)->isSuccess(); 
+    }
+    
+    private function markdown($text, $paragraph = false)
+    {
+        $parser = new Markdown();
+        
+        if ($paragraph) {
+            return $parser->parseParagraph($text);
+        }
+        
+        return $parser->parse($text);
+    }
+    
     public function actionInit()
     {
-        $wrapper = new GitWrapper();
+        // username
         $username = $this->getConfig('username');
-       
         if (!$username) {
-            $username = $this->prompt('Whats your username?');
+            $username = $this->prompt('Whats your Github username?');
             $this->saveConfig('username', $username);
         }
         
+        // clonetype
         $cloneType = $this->getConfig('cloneType');
         if (!$cloneType) {
             $cloneType = $this->select('Are you connected via ssh or https?', ['ssh' => 'ssh', 'http' => 'http']);
             $this->saveConfig('cloneType', $cloneType);
         }
         
+        $summary = [];
+        $itemWithoutFork = false;
+        
+        // generate summary overview
         foreach ($this->repos as $repo) {
-            $newRepoHome = 'repos' . DIRECTORY_SEPARATOR . $repo;
+            $newRepoHome = $this->getFilesystemRepoPath($repo);
             if (file_exists($newRepoHome . DIRECTORY_SEPARATOR . '.git')) {
-                $this->outputSuccess("repo: \"{$repo}\" already initalize.");
-                continue;
-            }
-            
-            $hasFork = (new Curl())->get('https://api.github.com/repos/'.$username.'/'.$repo)->isSuccess(); // will redirect to github page with 301
-            
-            if (!$hasFork) {
-                $this->outputInfo("We could not find a fork {$username}/{$repo}! We can setup a read only instance, you can't change. If you want to work on this module you should clone it first!");
-                
-                if ($this->confirm("proceed with read only repo for {$repo}.")) {
-                    $wrapper->cloneRepository('git://github.com/luyadev/'.$repo.'.git', $newRepoHome);
-                    $this->outputSuccess("Repo {$repo} cloned into repos");
-                    $cmd = $wrapper->git('remote add upstream https://github.com/luyadev/'.$repo.'.git',  $newRepoHome);
-                    $this->outputSuccess("add remote upstream.");
-                }
+                $summary[] = $this->summaryItem($repo, false, true);
+            } elseif ($this->forkExists($username, $repo)) {
+                $summary[] = $this->summaryItem($repo, true, false);
             } else {
-                
-                $typePrefix = ($cloneType == 'ssh') ? "git@github.com:{$username}/{$repo}.git" : "https://github.com/{$username}/{$repo}.git";
-                $this->outputInfo('clone ' . $typePrefix);
-                $wrapper->cloneRepository($typePrefix, $newRepoHome);
-                $this->outputSuccess("Repo {$repo} cloned into repos");
-                $cmd = $wrapper->git('remote add upstream https://github.com/luyadev/'.$repo.'.git',  $newRepoHome);
-                $this->outputSuccess("add remote upstream.");
+                $itemWithoutFork = true;
+                $summary[] = $this->summaryItem($repo, false, false);
             }
         }
         
-        return $this->outputSuccess("init complet.");
+        if ($itemWithoutFork) {
+            Console::clearScreen();
+            $this->outputInfo($this->markdown($this->text));
+            foreach ($summary as $sum) {
+                if (!$sum[2] && !$sum[1]) {
+                    $this->outputInfo($this->markdown("**{$sum[0]}**: https://github.com/luyadev/{$sum[0]}/fork", true));
+                }
+            }
+            echo (new Table())->setHeaders(['Repo', 'Already initialized', 'Fork exists'])->setRows($summary)->run();
+            $this->outputError("Repos without fork detected. Those repos will be initialized as READ ONLY. It means you can not push any changes to them.");
+            
+            if (!$this->confirm("Continue?")) {
+                return $this->outputError('Abort by User.');
+            }
+        }
+        
+        // foreach summary and clone
+        foreach ($summary as $sum) {
+            $repo = $sum[0];
+            $hasFork = $sum[2];
+            $exists = $sum[1];
+            
+            // continue already initialized repos.
+            if ($exists) {
+                continue;
+            }
+            
+            $newRepoHome = $this->getFilesystemRepoPath($repo);
+            
+            if ($hasFork) {
+                $cloneUrl = ($cloneType == 'ssh') ? "git@github.com:{$username}/{$repo}.git" : "https://github.com/{$username}/{$repo}.git";
+            } else {
+                $cloneUrl = ($cloneType == 'ssh') ? "git@github.com:luyadev/{$repo}.git" : "https://github.com/{$username}/{$repo}.git";
+            }
+            
+            $this->cloneRepo($repo, $cloneUrl, $newRepoHome);
+        }
+        
+        return $this->outputSuccess("init complete.");
+    }
+    
+    private function cloneRepo($repo, $cloneUrl, $newRepoHome)
+    {
+        $this->outputSuccess("{$repo}: cloning ...");
+        $this->getGitWrapper()->cloneRepository($cloneUrl, $newRepoHome);
+        $this->getGitWrapper()->git('remote add upstream https://github.com/luyadev/'.$repo.'.git',  $newRepoHome);
+        $this->outputSuccess("{$repo}: ✔ complete");
     }
     
     public function actionUpdate()
